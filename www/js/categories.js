863aa339943c76a2a9972e0ca46d70cbf972ba13
Rabbia Umer <rabbiajahangir92@gmail.com>
2017-04-21 12:38:42 +0500

    improvement(categories): minor changes in populating categories

--- www/js/categories.js
+++ www/js/categories.js
@@ -4,7 +4,6 @@
 
 $(function () {
   $(document).on("pagecreate", function (event) {
-    console.log("PAGECREATE");
     /* adding interval since jquery mobile allows to call hide/show loader events
      only on pageshow event or inside setInterval, refer to  http://stackoverflow.com/a/16277865/1609548*/
 
@@ -23,7 +22,7 @@ $(function () {
       success: function (languages) {
 
         languages.forEach(function (language) {
-          $("#categories").append('<div class="row"><div class="col-xs-12"> <button type="button" class="btn btn-default btn-block">' + language.name + ' </button></div></div>');
+          $("#categories").append('<div class="row mb1"><div class="col-xs-12"> <button type="button" class="btn btn-default btn-block">' + language.name.toUpperCase() + ' </button></div></div>');
         });
         /* adding interval since jquery mobile allows to call hide/show loader events
          only on pageshow event or inside setInterval, refer to  http://stackoverflow.com/a/16277865/1609548*/
