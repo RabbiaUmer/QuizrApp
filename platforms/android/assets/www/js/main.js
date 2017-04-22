680276f91d19043352101e2d13d8f680ae6c7c04
Rabbia Umer <rabbiajahangir92@gmail.com>
2017-04-22 05:47:02 +0500

    CHORE(.gitignore): remove gitignore file for now

---
+++ platforms/android/assets/www/js/main.js
@@ -0,0 +1,33 @@
+$(function () {
+    $(document).on("pagecreate", function (event) {
+        //Handling the login form
+        $("#login-form form").submit(function (event) {
+
+            //preveneting the default form submit event/behaviour/action
+            event.preventDefault();
+            var userEmail = $(this).find("#login-email").val();
+            var userPassword = $(this).find("#login-password").val();
+            $.ajax({
+                type: "POST",
+                url: 'http://localhost:8080/login',
+                data: {
+                    email: userEmail,
+                    password: userPassword
+                },
+                success: function (response) {
+                    console.log(response);
+                },
+                dataType: "json"
+            });
+        });
+
+//  Ends login form
+
+        $("#signup-form form").submit(function () {
+            console.log("submitting");
+            //preveneting the default form submit event/behaviour/action
+            event.preventDefault();
+
+        });
+    });
+});
---
+++ platforms/browser/www/js/categories.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Rabbia Umer on 9/19/16.
+ */
+
+$(function () {
+  $(document).on("pagecreate", function (event) {
+    /* adding interval since jquery mobile allows to call hide/show loader events
+     only on pageshow event or inside setInterval, refer to  http://stackoverflow.com/a/16277865/1609548*/
+
+    var showLoader = setInterval(function () {
+      $.mobile.loading('show');
+      clearInterval(showLoader);
+    });
+
+    $.ajax({
+      url: 'http://localhost:8000/categories',
+      type: 'GET',
+      error: function (err) {
+        console.log(err);
+      },
+      async: true,
+      success: function (languages) {
+
+        languages.forEach(function (language) {
+          $("#categories").append('<div class="row mb1"><div class="col-xs-12"> <button type="button" class="btn btn-default btn-block">' + language.name.toUpperCase() + ' </button></div></div>');
+        });
+        /* adding interval since jquery mobile allows to call hide/show loader events
+         only on pageshow event or inside setInterval, refer to  http://stackoverflow.com/a/16277865/1609548*/
+        var hideLoader = setInterval(function () {
+          $.mobile.loading('hide');
+          clearInterval(showLoader);
+        });
+
+      }
+    });
+  });
+});
