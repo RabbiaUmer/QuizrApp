caa51bec90e17e77ee67c754067f0df247981873
Rabbia Umer <rabbiajahangir92@gmail.com>
2017-04-26 13:46:28 +0500

    some changes

--- platforms/browser/www/js/categories.js
+++ platforms/browser/www/js/categories.js
@@ -11,10 +11,10 @@ $(function () {
     /* adding interval since jquery mobile allows to call hide/show loader events
      only on pageshow event or inside setInterval, refer to  http://stackoverflow.com/a/16277865/1609548*/
 
-    // var showLoader = setInterval(function () {
-    //   $.mobile.loading('show');
-    //   clearInterval(showLoader);
-    // });
+    var showLoader = setInterval(function () {
+      $.mobile.loading('show');
+      clearInterval(showLoader);
+    });
 
     $.ajax({
       url: 'http://localhost:8000/categories',
@@ -30,10 +30,10 @@ $(function () {
         });
         /* adding interval since jquery mobile allows to call hide/show loader events
          only on pageshow event or inside setInterval, refer to  http://stackoverflow.com/a/16277865/1609548*/
-        // var hideLoader = setInterval(function () {
-        //   $.mobile.loading('hide');
-        //   clearInterval(showLoader);
-        // });
+        var hideLoader = setInterval(function () {
+          $.mobile.loading('hide');
+          clearInterval(showLoader);
+        });
 
       }
     });
