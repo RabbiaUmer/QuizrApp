b30c1204b4e2c8166241a03e69e8850847382f8f
Rabbia Umer <rabbiajahangir92@gmail.com>
2017-05-28 06:29:00 -0400

    automatically generated files changes

--- platforms/android/assets/www/js/categories.js
+++ platforms/android/assets/www/js/categories.js
@@ -16,12 +16,18 @@ $(function () {
       clearInterval(showLoader);
     });
 
+    var token = window.localStorage.getItem('user-token');
+
     $.ajax({
       url: 'http://localhost:8000/categories',
+      headers: {
+        'x-access-token': token
+      },
       type: 'GET',
       error: function (err) {
         console.log(err);
       },
+      contentType: 'text/plain',
       async: true,
       success: function (languages) {
 
