0a87bf5f3e90a15be88388bd349e08a20873d643
Rabbia Umer <rabbiajahangir92@gmail.com>
2017-07-17 12:17:06 +0500

    lot of auto generated build files

--- platforms/android/assets/www/js/signup.js
+++ platforms/android/assets/www/js/signup.js
@@ -1,64 +1,57 @@
-//Handling the login form
-$('#signup-form form').submit(function (event) {
-  $("#signup-btn").prop("disabled", true);
+$(function () {
 
-  // if there is already error message added during previous attempt then remove that first
-  removeSignupError();
 
-  var userEmail = $(this).find("#signup-email").val();
-  var userPassword = $(this).find("#signup-password").val();
-  console.log(userEmail);
-  console.log(userPassword);
+  //Handling the login form
+  $('#login-form form').submit(function (event) {
 
-  $.ajax({
-    type: "POST",
-    url: 'https://programmingquizlearningapp-sqnmzjpjuz.now.sh/signup',
-    data: {
-      email: userEmail,
-      password: userPassword
-    },
-    success: function (res) {
-      console.log(res);
-      if (res.success === true) {
-        var localStorage = window.localStorage;
-        localStorage.setItem("user-token", res.token);
-        openHomeScreen();
-        $("#signup-btn").prop("disabled", false);
-      } else {
-        addSignupError(res.message);
-      }
-    },
-    dataType: "json"
+    // if there is already error message added during previous attempt then remove that first
+    removeLoginError();
+
+    var userEmail = $(this).find("#login-email").val();
+    var userPassword = $(this).find("#login-password").val();
+
+    $.ajax({
+      type: "POST",
+      url: 'http://localhost:8000/login',
+      // contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+      data: {
+        email: userEmail,
+        password: userPassword
+      },
+      success: function (res) {
+        if (res.success === true) {
+          var localStorage = window.localStorage;
+          localStorage.setItem("user-token", res.token);
+          openHomeScreen();
+        } else {
+          addLoginError();
+        }
+      },
+      dataType: "json"
+    });
+
+    event.preventDefault();
   });
 
-  event.preventDefault();
+
+  // Removing errors on focus
+  $('#login-form form').find("#login-email, #login-password").focus(function () {
+    // if there is already error message added during previous attempt then remove that on focus
+    removeLoginError();
+  });
+
+  // helper functions below (used in code above)
+  function openHomeScreen() {
+    $.mobile.navigate("/home.html");
+  }
+
+  function addLoginError() {
+    $('#login-form form')
+      .append('<p class="center login-error">Please check your email or password!</p>');
+  }
+
+  function removeLoginError() {
+    $('p.login-error').remove();
+  }
+
 });
-
-
-// Removing errors on focus
-$('#signup-form form').find("#signup-email-email, #signup-password").focus(function () {
-  // if there is already error message added during previous attempt then remove that on focus
-  removeSignupError();
-});
-
-// helper functions below (used in code above)
-function openHomeScreen() {
-  $(':mobile-pagecontainer').pagecontainer("change", "home.html", {
-    role: "page",
-    transition: "fade",
-    changeHash: true,
-    reverse: true,
-    showLoadMsg: true
-  })
-}
-
-function addSignupError(msg) {
-  $("#signup-btn").prop("disabled", true);
-  $('#signup-form form')
-    .append('<p class="center signup-error bg-danger">' + msg + '</p>');
-}
-
-function removeSignupError() {
-  $("#signup-btn").prop("disabled", false);
-  $('p.signup-error').remove();
-}
