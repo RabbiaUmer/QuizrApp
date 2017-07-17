fb678ef3383a1791a0d69138b417f355a6e4397c
Rabbia Umer <rabbiajahangir92@gmail.com>
2017-07-17 04:51:25 -0400

    remove .gitignore contents

---
+++ platforms/android/assets/www/js/login.js
@@ -0,0 +1,63 @@
+//Handling the login form
+$('#login-form form').submit(function (event) {
+  $("#login-btn").prop("disabled", true);
+
+  // if there is already error message added during previous attempt then remove that first
+  removeLoginError();
+
+  var userEmail = $(this).find("#login-email").val();
+  var userPassword = $(this).find("#login-password").val();
+
+  $.ajax({
+    type: "POST",
+    url: 'https://programmingquizlearningapp-sqnmzjpjuz.now.sh/login',
+    // contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    data: {
+      email: userEmail,
+      password: userPassword
+    },
+    success: function (res) {
+      if (res.success === true) {
+        var localStorage = window.localStorage;
+        localStorage.setItem("user-token", res.token);
+        openHomeScreen();
+        $("#login-btn").prop("disabled", false);
+      } else {
+        addLoginError();
+      }
+    },
+    dataType: "json"
+  });
+
+  event.preventDefault();
+});
+
+
+// Removing errors on focus
+$('#login-form form').find("#login-email, #login-password").focus(function () {
+  // if there is already error message added during previous attempt then remove that on focus
+  removeLoginError();
+});
+
+// helper functions below (used in code above)
+function openHomeScreen() {
+  $(':mobile-pagecontainer').pagecontainer("change", "home.html", {
+    role: "page",
+    transition: "fade",
+    changeHash: true,
+    reverse: true,
+    showLoadMsg: true
+  })
+}
+
+function addLoginError() {
+  $("#login-btn").prop("disabled", true);
+  $('<p class="center login-error bg-danger">Please check your email or password!</p>').hide().appendTo('#login-form form').slideDown(500);
+}
+
+function removeLoginError() {
+  $("#login-btn").prop("disabled", false);
+  $('p.login-error').slideUp(500, function () {
+    $('p.login-error').remove();
+  });
+}
---
+++ platforms/browser/www/js/home.js
@@ -0,0 +1,17 @@
+/**
+ * Created by rabbiaumer on 5/29/17.
+ */
+
+$(function () {
+  $("#logout-btn").on("click", function (event) {
+    event.preventDefault();
+    window.localStorage.removeItem("user-token");
+    $(':mobile-pagecontainer').pagecontainer("change", "login.html", {
+      role: "page",
+      transition: "fade",
+      changeHash: true,
+      reverse: true,
+      showLoadMsg: true
+    })
+  });
+});
