20e1e70599590ff3f341a86f9098d987e74aab97
Rabbia Umer <rabbiajahangir92@gmail.com>
2017-08-20 15:05:33 -0400

    use class instead of id, give id to the button, remove the loader element on start game button click

--- www/js/compete.js
+++ www/js/compete.js
@@ -20,15 +20,23 @@ $(function () {
   // });
 
   $("#compete-screen").on("pageshow", function () {
+
+    // Retreiving data from URL parameters
     var params = $(this).data("url").split("?")[1];
     var emptyString = "";
     var individualParams = params.split("&");
     var id = individualParams[0].replace("id=", emptyString);
     var name = individualParams[1].replace("name=", emptyString);
 
-    var competitionLoader = $("#competition").append("<div class='comp-loader'><div class='row'></div></div>");
+    // Showing the inital message and data
+    var competitionLoader = $("#competition").append("<div id='comp-loader'><div class='row'></div></div>");
+    competitionLoader.append("<div class='col-xs-12 text-center'> <h1 class='animated pulse'>" + name + "</h1> <div><p>Here we go!</p></div> <div><br><p> You will be presented with multiple choice questions, correct answers would increase your reputation.</p></div> <br> <br> <p>Press the button to start</p> <br> <br><button id='comp-start-btn' class='ui-btn ui-shadow animated infinite pulse'>Start Game</button></div>");
 
-    competitionLoader.append("<div class='col-xs-12 text-center'> <h1 class='animated pulse'>" + name + "</h1> <div><p>Here we go!</p></div> <div><br><p> You will be presented with multiple choice questions, correct answers would increase your reputation.</p></div> <br> <br> <p>Press the button to start</p> <br> <br><button class='ui-btn ui-shadow animated infinite pulse'>Start Game</button></div>");
+    // On start game button, remove the loader element (because we're going to show the questions on the same page
+    $('#comp-start-btn').on('click', function (event) {
+      event.preventDefault();
+      competitionLoader.remove();
+    });
 
   });
 
