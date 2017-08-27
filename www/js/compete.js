d0b5c97f4c7b825dfa4bdcaa207087aea76da679
Rabbia Umer <rabbiajahangir92@gmail.com>
2017-08-27 03:11:29 -0400

    i think i changed the animation for the language heading

--- www/js/compete.js
+++ www/js/compete.js
@@ -30,15 +30,12 @@ $(function () {
 
     // Showing the inital message and data
     var competitionLoader = $("#competition").append("<div id='comp-loader'><div class='row'></div></div>");
-    competitionLoader.append("<div class='col-xs-12 text-center'> <h1 class='animated pulse'>" + name + "</h1> <div><p>Here we go!</p></div> <div><br><p> You will be presented with multiple choice questions, correct answers would increase your reputation.</p></div> <br> <br> <p>Press the button to start</p> <br> <br><button id='comp-start-btn' class='ui-btn ui-shadow animated infinite pulse'>Start Game</button></div>");
+    competitionLoader.append("<div class='col-xs-12 text-center'> <h1 class='animated fadeInDown'>" + name + "</h1> <div><p>Here we go!</p></div> <div><br><p> You will be presented with multiple choice questions, correct answers would increase your reputation.</p></div> <br> <br> <p>Press the button to start</p> <br> <br><button id='comp-start-btn' class='ui-btn ui-shadow animated infinite pulse'>Start Game</button></div>");
 
     // On start game button, remove the loader element (because we're going to show the questions on the same page
     $('#comp-start-btn').on('click', function (event) {
       event.preventDefault();
       competitionLoader.remove();
     });
-
   });
-
-
 });
