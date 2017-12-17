105d8253ec071524b1df0b7879926ada0a5750f6
Rabbia Umer <rabbiajahangir92@gmail.com>
2017-12-17 15:01:11 -0500

    remove href, change anchor tag to button, add class to buttons, addtach page change event, because we want to notify categories page the which player games do we want to play

--- www/js/home.js
+++ www/js/home.js
@@ -9,13 +9,37 @@ $(function () {
   $.ajax({
     type: "GET",
     headers: {"x-access-token": authToken}, // have to send token on every request for authentication
-    url: serverUrl.hosted+'/user-profile',
+    url: serverUrl.hosted + '/user-profile',
     success: function (res) {
       $(".header").text(res.firstName + " " + res.lastName);
       $("#playerAvatar").attr("src", res.avatar);
     },
     dataType: "json"
   });
+
+  // Once the buttons have been added, attach the click event on them
+  $('.single-player-btn').on('click', function (event) {
+    event.preventDefault();
+    pageContainerChange('single');
+  });
+
+  $('.multiple-player-btn').on('click', function (event) {
+    event.preventDefault();
+    pageContainerChange('multiple');
+  });
+
+  function pageContainerChange(challengeType) {
+    $(':mobile-pagecontainer').pagecontainer("change", "categories.html", {
+      role: "page",
+      transition: "fade",
+      changeHash: true,
+      reverse: true,
+      showLoadMsg: true,
+      data: {
+        challenge: challengeType
+      }
+    })
+  }
 
 
   $("#logout-btn").on("click", function (event) {
